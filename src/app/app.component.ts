c1142f5141f472925b411a9d6586539710b5efc8
akshayibitwar <akshayebitwar@gmail.com>
2024-01-18 23:25:12 +0530

    Template-DrivenForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TamplateDrivenForm';
+@ViewChild("SignInForm") SignInForm ! : NgForm;
+secretquestionDefault : string = 'favbook';
+modeofContact = [
+  {moc : "byemail", id : 123},
+  {moc : "byphone", id : 124},
+];
+//   onSignUp(SignUpForm:NgForm){
+//       if(SignUpForm.valid){
+//         console.log(SignUpForm.value);
+//         SignUpForm.reset()
+//       }
+//   }
+//     ======================OR
+obj = 
+  {
+    "userInfo": {
+        "UserName": "akkiak123",
+        "Email": "asaa"
+    },
+    "secretquestion": "favteacher",
+    "answer": "jadhav",
+    "subscribToEmail": true,
+    "Contactmode": "byemail"
+}
+
+
+
+onSignUp(){
+    if(this.SignInForm.valid){
+      console.log(this.SignInForm.value);
+    this.SignInForm.reset();
+    }
+  }
+
+  onEditInfo(){
+      //this.SignInForm.form.patchValue(this.obj);
+      this.SignInForm.form.setValue(this.obj);
+  }
 }
